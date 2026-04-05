e58ebb2d39845bf4077de5bdc20c0b7b76d99243
Jazmín Gil <124318350+JazGil@users.noreply.github.com>
2026-04-05 22:01:46 +0000

    Estilos (Esto incluye Cargando los estilos, Estilos Globales, y CSS Modules)

--- nextjs-dashboard/app/layout.tsx
+++ nextjs-dashboard/app/layout.tsx
@@ -1,3 +1,5 @@
+import './ui/global.css';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,11 +7,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head />
       <body>
-        <h1> Titulo</h1>
-        <div style={{ border: '1px solid #000' }}></div>
+        <h1>Titulo</h1>
+        <div style={{ border: '1px solid #000', padding: '10px' }}>
+          
+        </div>
         {children}
-
       </body>
     </html>
   );
